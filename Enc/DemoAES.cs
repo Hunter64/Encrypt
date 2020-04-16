db8a9a65a979c3f0970ecd78741e6f25e2fe9ab9
Héctor López <hector.lopez@DSA-DEV2-10>
2020-04-16 17:49:01 -0500

    load project

---
+++ Enc/DemoAES.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Enc
+{
+    class DemoAES
+    {
+        AesCryptoServiceProvider AesCryptoServiceProvider;
+
+        public DemoAES()
+        {
+            AesCryptoServiceProvider = new AesCryptoServiceProvider
+            {
+                BlockSize = 128,
+                KeySize = 256
+            };
+            AesCryptoServiceProvider.GenerateIV();
+            AesCryptoServiceProvider.GenerateKey();
+            AesCryptoServiceProvider.Mode = CipherMode.CBC;
+            AesCryptoServiceProvider.Padding = PaddingMode.PKCS7;
+        }
+
+        public string Encrypt(String text)
+        {
+            ICryptoTransform cryptoTransform = AesCryptoServiceProvider.CreateEncryptor();
+            byte[] vs = cryptoTransform.TransformFinalBlock(ASCIIEncoding.ASCII.GetBytes(text), 0, text.Length);
+            string str = Convert.ToBase64String(vs);
+            return str;
+        }
+
+    }
+}
---
+++ Enc/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace Enc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Json MC examples
+            string strc = Get_Value();
+            string strc2 = "{\"AMO\":500,\"COM\":1,\"DAT\":\"1508230058706\",\"DES\":\"Venta menudeo\",\"IDC\":\"15f298452d2\",\"REF\":0,\"TYP\":19,\"v\":{\"ACC\":\"5872123456786012\",\"BAN\":\"40127\",\"DEV\":\"0754676270/8\",\"NAM\":\"Rafael Valenzuela Arenas\",\"TYC\":3}}";
+
+            DemoAES demoAES = new DemoAES();
+            Console.WriteLine(demoAES.Encrypt(strc2));
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine(demoAES.Encrypt(strc));
+            Console.ReadKey();
+        }
+
+        private static string Get_Value()
+        {            
+            string json = @"{'IDC': '15f298452d2',
+                'DES': 'Venta menudeo',
+                'AMO': 500,
+                'DAT': 1508230058706,'REF': 0,
+                'COM': 1,
+                'TYP': 19,
+                'v': {
+                'NAM': 'Rafael Valenzuela Arenas',
+                'ACC': '5872123456786012',
+                'BAN': 40127,
+                'TYC': 3,
+                'DEV': '0754676270/8'
+                }}";
+
+            var json_str = new JavaScriptSerializer().Serialize(json);
+
+            return json_str;
+
+        }
+    }
+}
