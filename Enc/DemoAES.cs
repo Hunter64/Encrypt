81f23408636256f98e1a616eda7db1dfaeca597f
Héctor López <hector.lopez@DSA-DEV2-10>
2020-04-21 08:50:46 -0500

    encr

--- Enc/DemoAES.cs
+++ Enc/DemoAES.cs
@@ -13,13 +13,18 @@ namespace Enc
 
         public DemoAES()
         {
+            byte[] key = { 63, 145, 10, 166, 158, 126, 221, 10, 161, 193, 40, 94, 221, 6, 226, 252 };
+            byte[] IV = { 161, 238, 253, 121, 130, 149, 95, 147, 101, 121, 230, 136, 23, 11, 173, 6 };
+
             AesCryptoServiceProvider = new AesCryptoServiceProvider
             {
                 BlockSize = 128,
-                KeySize = 256
+                KeySize = 128,
+                Key = key,
+                IV = IV
             };
-            AesCryptoServiceProvider.GenerateIV();
-            AesCryptoServiceProvider.GenerateKey();
+            //AesCryptoServiceProvider.GenerateIV();
+            //AesCryptoServiceProvider.GenerateKey();
             AesCryptoServiceProvider.Mode = CipherMode.CBC;
             AesCryptoServiceProvider.Padding = PaddingMode.PKCS7;
         }
--- Enc/Program.cs
+++ Enc/Program.cs
@@ -14,11 +14,14 @@ namespace Enc
             //Json MC examples
             string strc = Get_Value();
             string strc2 = "{\"AMO\":500,\"COM\":1,\"DAT\":\"1508230058706\",\"DES\":\"Venta menudeo\",\"IDC\":\"15f298452d2\",\"REF\":0,\"TYP\":19,\"v\":{\"ACC\":\"5872123456786012\",\"BAN\":\"40127\",\"DEV\":\"0754676270/8\",\"NAM\":\"Rafael Valenzuela Arenas\",\"TYC\":3}}";
+            string stringify = "EOLSJWGSjTk69A5Eque6UFl+ozsNMbJCJzcLuaot4X7AgyNX30URLVYCPYKJvYsIuwgvNprNZTdcRulMjOVBRetbolqkPO2ZjmXiR66OmHHI9I1wDHt1maQD7uzFDlllSGRr6VyNNK4Lq/N1XoBlODiUzi/o5zPCxTygXOGJ7MI94BTU90xaOe8RVojN/obKbXAACiUbjuqCWq2wbSYEhZcibdTXHmf1GkGQb/y9sD2K51zLO+4pFxF/YUoV/lbvXF4ZeojERCmu4Yop4x+UJUKl/MTY2X7MYqd0nIrgnQuz8wNAgxGF7Ub2uGJ90W0l3i/FSmxpopLUGRiTY3ZHABplg6H77povwD42SPsSX/3E62rXXcal4Mf/j1m8r0K1HQwc5TdG";
 
             DemoAES demoAES = new DemoAES();
+            Console.WriteLine(demoAES.Encrypt(strc));
+            Console.WriteLine("------------------------------------------------------------ Éste es el bueno -> ");
             Console.WriteLine(demoAES.Encrypt(strc2));
             Console.WriteLine("------------------------------------------------------------");
-            Console.WriteLine(demoAES.Encrypt(strc));
+            Console.WriteLine(demoAES.Encrypt(stringify));
             Console.ReadKey();
         }
 
